6bfdaa0d46a017b5fa32a32d6d9d603feec52086
ashwini <ashwiniyadav23@navgurukul.org>
2025-01-03 19:11:46 +0530

    upsate changing code

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
+
 button.addEventListener('click', () => {
     const li = document.createElement('li');
+
     li.textContent = input.value;
     ul.appendChild(li);
     input.value = '';
     input.focus();
+    localStorage.setItem('items', ul.innerHTML);
+
     });
